654cf9be6dca1657907bf207569fb7a5fb2b89a2
Rike-Benjamin Schuppner <rikebs@debilski.de>
2014-08-01 22:50:34 +0200

    ENH: Disable the div wrapper from pygments. Generate everything with blaze.

--- debilski-de.hs
+++ debilski-de.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.List.Utils (split,join)
 import           Control.Applicative
+import           Control.Monad ((>=>),guard)
 import           Data.Monoid (mappend, mconcat)
 import           Hakyll
 import qualified Text.Pandoc.Options as P
@@ -10,6 +11,11 @@ import Text.Pandoc.Definition
 import Text.Pandoc.Walk
 
 import Data.Char(toLower)
+
+import qualified Text.Blaze as B
+import qualified Text.Blaze.Html5 as H5
+import qualified Text.Blaze.Html5.Attributes as H5A
+import Text.Blaze.Renderer.String
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -111,23 +117,43 @@ pandocOptions = defaultHakyllWriterOptions { P.writerHtml5 = True
                                            , P.writerHTMLMathMethod = P.MathML (Just "") } -- P.MathJax ""
 
 applyPygments :: Pandoc -> Compiler Pandoc
-applyPygments = walkM changeBlocks
+applyPygments = walkM changeBlocks >=> walkM changeInline
   where
     changeBlocks :: Block -> Compiler Block
-    changeBlocks (CodeBlock (_, options, _) code) = RawBlock "html" <$> pygments code options
+    changeBlocks (CodeBlock attrs@(_, cls, _) code) = RawBlock "html" <$> (wrapPre attrs <$> pygments cls code)
     changeBlocks x = return x
 
+    changeInline :: Inline -> Compiler Inline
+    changeInline (Code attrs@(_, cls@(_:_), _) code) = RawInline "html" <$> (wrapCode attrs <$> pygments cls code)
+    changeInline x = return x
+
+    setAttrs (id_, cls, kv) tag = foldl (H5.!) tag attributes
+      where
+        kv' = case cls of
+                (lang : _) -> ("data-language", lang) : kv
+                _ -> kv
+        attributes :: [B.Attribute]
+        attributes = concat [ [ H5A.class_ (B.toValue $ mkClasses cls) ]
+                            , H5A.id (B.toValue id_) <$ guard (not $ null id_)
+                            , fmap (\(k, v) -> B.customAttribute (B.stringTag k) (B.toValue v)) kv'
+                            ]
+
+    wrapCode attrs code = renderMarkup $ setAttrs attrs H5.code (H5.preEscapedToHtml code)
+    wrapPre attrs code = renderMarkup $ setAttrs attrs H5.div $ H5.pre (H5.preEscapedToHtml code)
+    mkClasses (lang:other) = join " " ("highlight" : ("language-" ++ lang) : other)
+    mkClasses _ = "highlight"
+
 -- https://gist.github.com/fizruk/6620756 adapted and improved
-pygments:: String -> [String] -> Compiler String
-pygments code options = unixFilter "pygmentize" pygOptions code
-                        -- return $ "<div class =\"highlight\"><pre>" ++ code ++ "</pre></div>"
+pygments :: [String] -> String -> Compiler String
+pygments cls = unixFilter "pygmentize" (pygOptions cls)
   where
-    pygOptions = ["-l", lang, "-f", format, "-O", "cssclass=" ++ cssClasses] ++ otherOpts
-    lang       = toLower <$> head options
-    format     = "html"
-    cssClasses = join " " ("highlight" : tail options)
-    otherOpts  = if "inline_linenos" `elem` options then ["-O", "linenos=inline"]
-                   else []
+    pygOptions []       = ["-g", "-f", "html"]
+    pygOptions (lang:_) = ["-l", toLower <$> lang, "-f", "html"] ++ args
+      where
+        mkOpts     = concatMap $ \o -> ["-O", o]
+        args       = concat [ mkOpts ["linenos=inline"] <* guard ("inline_linenos" `elem` cls)
+                            , mkOpts ["nowrap"]
+                            ]
 
 bundleFilter :: String -> [String] -> String -> Compiler String
 bundleFilter cmd args = unixFilter "bundle" (["exec", cmd] ++ args)
