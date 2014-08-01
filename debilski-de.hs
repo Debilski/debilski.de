ae4b29aad5b6c4e391ce656bf1afda199a049b98
Rike-Benjamin Schuppner <rikebs@debilski.de>
2014-08-01 16:22:16 +0200

    ENH: Correct HTML5 time attribute.

--- debilski-de.hs
+++ debilski-de.hs
@@ -131,19 +131,30 @@ pygments code options = unixFilter "pygmentize" pygOptions code
 bundleFilter :: String -> [String] -> String -> Compiler String
 bundleFilter cmd args = unixFilter "bundle" (["exec", cmd] ++ args)
 
-fileHistory :: FilePath -> Compiler [(String, String, String)]
-fileHistory fp = extractHistory <$> unixFilter "git" ["log", "--format=%h%x09%aD%x09%s", "--", fp] ""
+data FileHistory = FileHistory { _hash :: String
+                               , _isodate :: String
+                               , _date :: String
+                               , _log :: String
+                               }
+
+fileHistory :: FilePath -> Compiler [FileHistory]
+fileHistory fp = extractHistory <$> unixFilter "git" ["log", "--format=%h%x09%ai%x09%aD%x09%s", "--", fp] ""
   where
     extractHistory s = fmap (extractLine . split "\009") (lines s)
-    extractLine (hash : date : log : _) = (hash, date, log)
-    extractLine _ = ("unknown", "unknown", "unknown")
+    extractLine (hash : isodate : date : log : _) = FileHistory { _hash=hash, _isodate=isofix, _date=date, _log=log }
+      where
+        isofix = d ++ " " ++ t ++ zone
+        d : t : zone : _ = split " " isodate
+    extractLine _ = FileHistory { _hash="unknown", _isodate="unknown", _date="unknown", _log="unknown" }
 
 historyContext :: FilePath -> Context b
 historyContext fp = listField "history" fields items
   where
     items = fileHistory fp >>= mapM makeItem
-    fields = mconcat [historyField "histhash" hash, historyField "histlog" log, historyField "histdate" date]
+    fields = mconcat [ historyField "histhash" _hash
+                     , historyField "histisodate" _isodate
+                     , historyField "histdate" _date
+                     , historyField "histlog" _log
+                     ]
     historyField name v = field name (return . v . itemBody)
-    hash (a, _, _) = a
-    date (_, a, _) = a
-    log (_, _, a) = a
+
